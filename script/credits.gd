4e19cbf7816eb63bbf1ea214b0fe8e8a8afa3f5b
Elmer Estoque <destoque.k12151881@umak.edu.ph>
2025-11-26 14:35:43 +0800

    asfas

--- script/credits.gd
+++ script/credits.gd
@@ -1,15 +1,10 @@
 extends CanvasLayer
-@onready var video_player: VideoStreamPlayer = $VideoStreamPlayer
+
+@onready var video_player: VideoStreamPlayer = $Control/VideoStreamPlayer
 
 func _ready() -> void:
 	MusicManager.stop_song()
 	video_player.finished.connect(_on_video_finished)
-	
-	video_player.set_anchors_preset(Control.PRESET_FULL_RECT)
-	video_player.size = get_viewport().get_visible_rect().size
-
-func _process(delta: float) -> void:
-	pass
 
 func _on_video_finished() -> void:
 	MusicManager.play_song("menu")
